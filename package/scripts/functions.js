7a0486a332895ea6a5f630a5216127dd7a445e6e
Alejandro Greggio <agreggio@slingr.io>
2023-11-12 17:59:48 -0300

    #1 - Migrate stripe endpoint to package

--- package/scripts/functions.js
+++ package/scripts/functions.js
@@ -209,9 +209,9 @@ function setApiUri(options) {
 
 function setRequestHeaders(options) {
     var headers = options.headers || {};
-    if (config.get("authenticationMethod") === "apiKey") { // TODO: Set the authentication method, if needed or remove this if (Remove comments after set the url)
+    if (config.get("authenticationMethod") === "apiKey") {
         sys.logs.debug('[stripe] Set header apikey');
-        headers = mergeJSON(headers, {"Authorization": "API-Key " + config.get("secretKey")});
+        headers = mergeJSON(headers, {"Authorization": "Bearer " + config.get("secretKey")});
     } 
     headers = mergeJSON(headers, {"Content-Type": "application/json"});
 
