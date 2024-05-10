266783f3caa20c27026db483e4a32eb3745c8c29
pasaperez-slingr <119679724+pasaperez-slingr@users.noreply.github.com>
2024-05-10 10:46:24 -0300

    Update api.js

--- scripts/api.js
+++ scripts/api.js
@@ -20,11 +20,7 @@ let httpService = {};
  * Handles a request with retry from the platform side.
  */
 function handleRequestWithRetry(requestFn, options, callbackData, callbacks) {
-    try {
-        return requestFn(options, callbackData, callbacks);
-    } catch (error) {
-        sys.logs.info("[stripe] Handling request "+JSON.stringify(error));
-    }
+    return requestFn(options, callbackData, callbacks);
 }
 
 function createWrapperFunction(requestFn) {
